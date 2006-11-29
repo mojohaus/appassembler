aa4f7216843b4ce707cbfeb3f550707561140a54
Kristian Nordal <kristian.nordal@gmail.com>
2006-11-29 21:24:11 +0000

    o Added "Flat" implememtation of the ArtifactRepositoryLayout, in order to support a flat lib structure for the dependencies o Updated CreateRepositoryMojo to support flat repository layout
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/appassembler@2691 52ab4f32-60fc-0310-b215-8acea882cd1b

--- appassembler-maven-plugin/src/main/java/org/codehaus/mojo/appassembler/CreateRepositoryMojo.java
+++ appassembler-maven-plugin/src/main/java/org/codehaus/mojo/appassembler/CreateRepositoryMojo.java
@@ -8,8 +8,10 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
 import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.artifact.repository.layout.LegacyRepositoryLayout;
 import org.apache.maven.artifact.installer.ArtifactInstallationException;
 import org.apache.maven.artifact.installer.ArtifactInstaller;
+import org.codehaus.mojo.appassembler.repository.FlatRepositoryLayout;
 
 import java.io.File;
 import java.util.Iterator;
@@ -18,12 +20,11 @@ import java.util.Set;
 /**
  * Creates an appassembler repository.
  *
+ * @author <a href="mailto:kristian.nordal@gmail.com">Kristian Nordal</a>
+ * @version $Id$
  * @goal create-repository
  * @requiresDependencyResolution runtime
  * @phase package
- *
- * @author <a href="mailto:kristian.nordal@gmail.com">Kristian Nordal</a>
- * @version $Id$
  */
 public class CreateRepositoryMojo
     extends AbstractMojo
@@ -41,6 +42,24 @@ public class CreateRepositoryMojo
      */
     private File assembleDirectory;
 
+
+    /**
+     * The directory that will be used for the dependencies, relative to
+     * assembleDirectory.
+     *
+     * @required
+     * @parameter expression="repo"
+     */
+    private String repoPath;
+
+    /**
+     * The layout of the generated Maven repository.
+     * Supported types - "default" (Maven2) | "legacy" (Maven1) | "flat" (flat lib/ style)
+     *
+     * @parameter default="default'
+     */
+    private String repositoryLayout;
+
     // -----------------------------------------------------------------------
     // Read-only parameters
     // -----------------------------------------------------------------------
@@ -56,7 +75,7 @@ public class CreateRepositoryMojo
      * @parameter expression="${localRepository}"
      */
     private ArtifactRepository localRepository;
-    
+
     /**
      * @readonly
      * @parameter expression="${project.build.directory}/${project.build.finalName}.${project.packaging}"
@@ -104,22 +123,39 @@ public class CreateRepositoryMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
+        // ----------------------------------------------------------------------
+        // Create new repository for dependencies
+        // ----------------------------------------------------------------------
+
+        if ( repositoryLayout == null || repositoryLayout.equals( "default" ) )
+        {
+            artifactRepositoryLayout = new DefaultRepositoryLayout();
+        }
+        else if ( repositoryLayout.equals( "legacy" ) )
+        {
+            artifactRepositoryLayout = new LegacyRepositoryLayout();
+        }
+        else if ( repositoryLayout.equals( "flat" ) )
+        {
+            artifactRepositoryLayout = new FlatRepositoryLayout();
+        }
+        else
+        {
+            throw new MojoFailureException( "Unknown repository layout '" + repositoryLayout + "'." );
+        }
+
         // -----------------------------------------------------------------------
         // Initialize
         // -----------------------------------------------------------------------
 
-        artifactRepositoryLayout = new DefaultRepositoryLayout();
-
         artifactRepository = artifactRepositoryFactory.createDeploymentArtifactRepository( "appassembler",
-            "file://" + assembleDirectory.getAbsolutePath() + "/repo", artifactRepositoryLayout, false );
+            "file://" + assembleDirectory.getAbsolutePath() + "/" + repoPath, artifactRepositoryLayout, false );
 
         // -----------------------------------------------------------------------
         // Install the project's artifact in the new repository
         // -----------------------------------------------------------------------
 
-        File projectArtifactFile = new File( artifactFinalName );
-
-        installArtifact( projectArtifact, projectArtifactFile );
+        installArtifact( projectArtifact );
 
         // ----------------------------------------------------------------------
         // Install dependencies in the new repository
@@ -129,25 +165,24 @@ public class CreateRepositoryMojo
         {
             Artifact artifact = (Artifact) it.next();
 
-            File artifactFile = new File( localRepository.getBasedir(), localRepository.pathOf( artifact ) );
-
-            installArtifact( artifact, artifactFile );
+            installArtifact( artifact );
         }
     }
 
-    private void installArtifact( Artifact artifact, File artifactFile )
+    private void installArtifact( Artifact artifact )
         throws MojoExecutionException
     {
-        if ( artifactFile.exists() )
+        try
         {
-            try
-            {
-                artifactInstaller.install( artifactFile, artifact, artifactRepository );
-            }
-            catch ( ArtifactInstallationException e )
-            {
-                throw new MojoExecutionException( "Failed to copy artifact.", e );
-            }
+            // Necessary for the artifact's baseVersion to be set correctly
+            // See: http://mail-archives.apache.org/mod_mbox/maven-dev/200511.mbox/%3c437288F4.4080003@apache.org%3e
+            artifact.isSnapshot();
+
+            artifactInstaller.install( artifact.getFile(), artifact, artifactRepository );
+        }
+        catch ( ArtifactInstallationException e )
+        {
+            throw new MojoExecutionException( "Failed to copy artifact.", e );
         }
     }
 }
