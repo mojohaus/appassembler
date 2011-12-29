89bbdae3c3249a0a528e41e0dccd675f97d29e15
Karl Heinz Marbaise <khmarbaise@apache.org>
2011-12-29 20:00:30 +0000

    [MAPPASM-135]   - Followup to r15645 added missing integration test.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/appassembler@15647 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ appassembler-maven-plugin/src/it/binFolderTest/verify.groovy
@@ -0,0 +1,50 @@
+import java.io.*
+import java.util.*
+
+
+t = new IntegrationBase();
+
+//The bin folder where to find the generated scripts.
+def fileBinFolder = new File( basedir, "target/appassembler/xbin");
+
+// Check the existence of the generated unix script
+def unixScriptFile = new File( fileBinFolder, "basic-test" );
+
+t.checkExistenceAndContentOfAFile(unixScriptFile, [
+    '[ -f "$BASEDIR"/xbin/setup-env ] && . "$BASEDIR"/xbin/setup-env',
+    'CLASSPATH=$CLASSPATH_PREFIX:"$BASEDIR"/etc:"$REPO"/org/codehaus/mojo/appassembler-maven-plugin/it/envrionmentSetup-test/1.0-SNAPSHOT/envrionmentSetup-test-1.0-SNAPSHOT.jar',
+    'EXTRA_JVM_ARGUMENTS="-Xms16m"',
+    'exec "$JAVACMD" $JAVA_OPTS \\',
+    '  $EXTRA_JVM_ARGUMENTS \\',
+    '  -classpath "$CLASSPATH" \\',
+    '  -Dapp.name="basic-test" \\',
+    '  -Dapp.pid="$$" \\',
+    '  -Dapp.repo="$REPO" \\',
+    '  -Dbasedir="$BASEDIR" \\',
+    '  org.codehaus.mojo.appassembler.example.helloworld.HelloWorld \\',
+    '  arg1 arg2 "$@"',
+    
+])
+
+File windowsBatchFile = new File( fileBinFolder, "basic-test.bat" );
+
+t.checkExistenceAndContentOfAFile(windowsBatchFile, [
+    'if exist "%BASEDIR%\\xbin\\setup-env.bat" call "%BASEDIR%\\xbin\\setup-env.bat"',
+    'set CLASSPATH="%BASEDIR%"\\etc;"%REPO%"\\org\\codehaus\\mojo\\appassembler-maven-plugin\\it\\envrionmentSetup-test\\1.0-SNAPSHOT\\envrionmentSetup-test-1.0-SNAPSHOT.jar',
+    'set EXTRA_JVM_ARGUMENTS=-Xms16m',
+    '%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="basic-test" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" org.codehaus.mojo.appassembler.example.helloworld.HelloWorld arg1 arg2 %CMD_LINE_ARGS%',
+])
+
+//Check the existence of the generated repository.
+def fileRepoFolder = new File( basedir, "target/appassembler/repo");
+
+if ( !fileRepoFolder.canRead() ) {
+    throw new FileNotFoundException( "Could not find the generated repository. " + fileRepoFolder );
+}
+
+def jarFileRepoFolder = new File( fileRepoFolder, "org/codehaus/mojo/appassembler-maven-plugin/it/envrionmentSetup-test/1.0-SNAPSHOT/envrionmentSetup-test-1.0-SNAPSHOT.jar");
+if ( !jarFileRepoFolder.canRead() ) {
+    throw new FileNotFoundException( "Could not find the generated jar. " + jarFileRepoFolder );
+}
+
+return true;
