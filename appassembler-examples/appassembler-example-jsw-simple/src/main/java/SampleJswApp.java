55133844804474ddfd38ae26b45f3bf5ff9e6324
Dan Tran <dantran@gmail.com>
2012-06-19 21:10:52 +0000

    [MAPPASM-176] add simple jsw app
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/appassembler@16915 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ appassembler-examples/appassembler-example-jsw-simple/src/main/java/SampleJswApp.java
@@ -0,0 +1,15 @@
+public class SampleJswApp {
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args ) {
+
+        try {
+			System.out.println( "Press control-C to terminate" );
+            Thread.sleep(  10000000000L );
+        }
+        catch ( Exception e ) {
+        }
+    }
+}
