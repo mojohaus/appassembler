5157e23c121b5d75216423dfcfa59f8326a569dc
Dan Tran <dantran@gmail.com>
2012-06-25 07:11:02 +0000

    [MAPPASM-178] add more test for useTimestampInSnapshotFilename configuration
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/appassembler@16970 52ab4f32-60fc-0310-b215-8acea882cd1b

--- appassembler-maven-plugin/src/it/useTimestampInSnapshotFileNameTest/verify.groovy
+++ appassembler-maven-plugin/src/it/useTimestampInSnapshotFileNameTest/verify.groovy
@@ -43,4 +43,97 @@ if ( !jarFile2.canRead() ) {
     throw new FileNotFoundException( "Generated jar found! " + jarFile1 );
 }
 
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+def repoFolder3 = new File( basedir, "target/exec-3/repo");
+def jarFile3 = new File( repoFolder3, "dbupgrade-core-1.0-beta-3-SNAPSHOT.jar" );
+
+if ( !jarFile3.canRead() ) {
+    throw new FileNotFoundException( "Generated jar found! " + jarFile1 );
+}
+
+//The bin folder where to find the generated scripts.
+def fileBinFolder3 = new File( basedir, "target/exec-3/bin");
+
+// Check the existence of the generated unix script
+def unixScriptFile3 = new File( fileBinFolder3, "useTimestampInSnapshotFileNameTest" );
+
+if ( !unixScriptFile3.canRead() ) {
+    throw new FileNotFoundException( "Could not find: " + unixScriptFile3 );
+}
+
+// Check the existence of the generated windows script
+File windowsBatchFile3 = new File( fileBinFolder3, "useTimestampInSnapshotFileNameTest.bat" );
+if ( !windowsBatchFile3.canRead() ) {
+    throw new FileNotFoundException( "Could not find: " + windowsBatchFile3 );
+}
+
+def list_of_files_which_must_be_in_classpath3 = [
+    'dbupgrade-core-1.0-beta-3-SNAPSHOT.jar'
+];
+
+
+def line_unix_script_with_classpath3 = "";
+unixScriptFile3.eachLine {
+    line_content, line_number -> if (line_content.startsWith('CLASSPATH=$CLASSPATH_PREFIX:"$BASEDIR"/etc')) {
+        line_unix_script_with_classpath3 = line_content;
+    }
+}
+
+list_of_files_which_must_be_in_classpath3.each {
+    lib_file -> if (!line_unix_script_with_classpath3.contains(lib_file)) {
+        throw new FileNotFoundException("We couldn't found " + lib_file + " in classpath (unix script).");
+    }
+}
+
+def line_windows_script_with_classpath3 = "";
+windowsBatchFile3.eachLine {
+    line_content, line_number -> if (line_content.startsWith(/set CLASSPATH="%BASEDIR%"\etc;/)) {
+        line_windows_script_with_classpath3 = line_content;
+    }
+}
+
+list_of_files_which_must_be_in_classpath3.each {
+    lib_file -> if (!line_windows_script_with_classpath3.contains(lib_file)) {
+        throw new FileNotFoundException("We couldn't found " + lib_file + " in classpath (windows script).");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+def repoFolder4 = new File( basedir, "target/exec-4/jsw/useTimestampInSnapshotFileNameTest/lib");
+def jarFile4 = new File( repoFolder4, "dbupgrade-core-1.0-beta-3-SNAPSHOT.jar" );
+
+if ( !jarFile4.canRead() ) {
+    throw new FileNotFoundException( "Generated jar found! " + jarFile4 );
+}
+
+//The bin folder where to find the generated scripts.
+def fileConfFolder4 = new File( basedir, "target/exec-4/jsw/useTimestampInSnapshotFileNameTest/conf");
+
+// Check the existence of the generated wrapper.conf script
+def wrapperConfFile4 = new File( fileConfFolder4, "wrapper.conf" );
+
+if ( !wrapperConfFile4.canRead() ) {
+    throw new FileNotFoundException( "Could not find: " + wrapperConf4 );
+}
+
+wrapperConfFile4.eachLine {
+    file_content -> if (file_content.startsWith('wrapper.java.classpath.3')) {
+        def found = false;
+        list_of_files_which_must_be_in_classpath3.each {
+            lib_file -> if (file_content.contains(lib_file)) {
+                found = true;
+            }
+            System.out.println( file_content );
+        }
+        if (!found) {
+            //TODO: Make it better!
+            throw new FileNotFoundException("We couldn't find an element!");
+        }
+    }
+}
+
 return true;
