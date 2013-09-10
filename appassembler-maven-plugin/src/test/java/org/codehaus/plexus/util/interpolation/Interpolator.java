413ba07d2ac3dee365898e5a074b9f5ed1bb0e6d
Dennis Lundberg <dennisl@apache.org>
2013-09-10 08:43:56 +0000

    Set Subversion properties.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/appassembler@18707 52ab4f32-60fc-0310-b215-8acea882cd1b

--- appassembler-maven-plugin/src/test/java/org/codehaus/plexus/util/interpolation/Interpolator.java
+++ appassembler-maven-plugin/src/test/java/org/codehaus/plexus/util/interpolation/Interpolator.java
@@ -3,7 +3,7 @@ package org.codehaus.plexus.util.interpolation;
 /**
  * COPIED FROM plexus-utils-1.5.15 TO SATISFY TESTS
  * 
- * @version $Id: RegexBasedInterpolator.java 12174 2010-05-16 21:04:35Z rfscholte $
+ * @version $Id$
  * @deprecated Use plexus-interpolation APIs instead.
  */
 public interface Interpolator
